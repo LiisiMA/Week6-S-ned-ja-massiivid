d7bd386c34e7e10e2818664e0b5d102357aba978
Liisi <lmarkus@tktk.ee>
2022-01-03 21:28:25 +0200

    Control Flow

---
+++ FirstOrLast/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FirstOrLast
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada ees- ja perekonnanime
+            //programm kontrollib andmete pikkust
+            //programm kuvab kumb nendest on pikem, kas ees- või perekonnanimi
+
+            Console.WriteLine("Sisesta oma eesnimi:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Sisesta oma perenimi:");
+            string lastname = Console.ReadLine();
+
+            if (firstName.Length > lastname.Length)
+            {
+                Console.WriteLine(firstName);
+            } else if (firstName.Length == lastname.Length) { Console.WriteLine("Võrdne " + firstName + " " + lastname); } 
+            else Console.WriteLine(lastname);
+        }
+    }
+}
---
+++ HOLCounter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HOLCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm kuvab konsoolis, mitu 'h', 'o' ja 'l' tähte on lauses "Hello World!"
+            string fullName = "Hello World!".ToLower();
+
+            int hCounter = 0;
+            int oCounter = 0;
+            int lCounter = 0;
+
+            for (int i = 0; i < fullName.Length; i++)
+            {
+
+                switch (fullName[i].ToString())
+                {
+                    case "h":
+                        hCounter++;
+                        break;
+                    case "o":
+                        oCounter++;
+                        break;
+                    case "l":
+                        lCounter++;
+                        break;
+                    default:
+                        break;
+                }
+                
+                //if (fullName[i] == 'h') { hCounter++; }
+                //if (fullName[i] == 'o') { oCounter++; }
+                //if (fullName[i] == 'l') { lCounter++; }
+            }
+
+                Console.WriteLine($"Lauses on {hCounter} h tähte, {oCounter} o tähte, {lCounter} l tähte");
+        }
+    }
+}
